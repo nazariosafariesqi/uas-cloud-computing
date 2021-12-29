865b5ec7520f86e79fb2d84f3f8fe3a52b6af528
Nazario Safariesqi <nazariotyo@gmail.com>
2021-12-29 10:41:14 +0700

    UAS

---
+++ includes/config.php
@@ -0,0 +1,20 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| OWSA-INV V2
+|--------------------------------------------------------------------------
+| Author: Siamon Hasan
+| Project Name: OSWA-INV
+| Version: v2
+| Offcial page: http://oswapp.com/
+| facebook Page: https://www.facebook.com/oswapp
+|
+|
+|
+*/
+  define( 'DB_HOST', '10.0.0.220' );          // Set database host
+  define( 'DB_USER', 'nazario' );             // Set database user
+  define( 'DB_PASS', 'Nazario02589.' );             // Set database password
+  define( 'DB_NAME', 'uas' );        // Set database name
+
+?>
